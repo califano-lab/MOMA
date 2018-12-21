86be5308356edd062769625743ba32a384e21e25
epaull <ep2835@c2b2.columbia.edu>
2018-12-21 15:52:58 -0500

    --

--- moma/R/models.match.r
+++ moma/R/models.match.r
@@ -18,6 +18,7 @@ model_viperMatcher <- setRefClass("model_viperMatcher", fields=
 	checkpoints="list",
 	clusters="list",
 	vpsim="matrix",
+	models.match="list",
 	CP.enrichments="matrix"),
 	methods = list(
 		#' 
@@ -37,9 +38,95 @@ model_viperMatcher <- setRefClass("model_viperMatcher", fields=
 			colnames(res) <- colnames(joint.matrix)
 			vpsim <<- res
 		}, 
+		matchingPatients = function(match.threshold=as.numeric("1e-5")) {
+
+			# for each patient cluster, match 
+			# determine sample order
+			best.cline.models <- list()
+			for (clus.id in 1:length(clusters)) {
+
+				# patient samples
+				clus.samples <- clusters[[clus.id]]
+
+				# these cell lines matche the checkpoint
+				cp.pvals <- 1-pnorm(matchObj$CP.enrichments[clus.id,])
+				cp.matches <- names(cp.pvals[p.adjust(cp.pvals, method='BH') < 0.05])
+
+				if (length(cp.matches)==0) {
+					print (paste("Warning, didn't find any matches to the checkpoint for cluster ", clus.id))
+					print ("Using viperSimilarity() only!")
+					cp.matched <- colnames(model.viper)
+				}
+
+				# count the number of patients each model system matches, according to viperSimiliarity()
+				# at the specified p-value threshold
+				number.patients.matched <- sapply(cp.matches, function(cline) {
+					patient.scores <- vpsim[clus.samples, cline]
+					pvals <- 1-pnorm(patient.scores)
+					length(pvals[which(pvals < match.threshold)])
+				})
+
+				ranked.models <- sort(number.patients.matched, dec=T)
+				best.cline.models[[clus.id]] <- ranked.models
+			}
+			models.match <<- best.cline.models
+	
+		}, 
 		#' Plot the viper matrix of the best matching cell lines against the 
 		#' 
-		plot.bestMatch.heatmap = function(output.file) {
+		heatmap.patientCoverage = function(models.match, output.file, top.clines=3) {
+
+			# features to select
+			features <- unlist(checkpoints)
+
+			# scale the VIPER patient matrix for display
+			zoom.vipermat <- c()
+			for (clus.id in 1:length(clusters)) {
+				clus.samples <- clusters[[clus.id]]
+				zoom.vipermat <- cbind(zoom.vipermat, patient.viper[features,clus.samples])
+			}
+			scaled.vipermat <- t(scale(t(zoom.vipermat), center=F))
+
+			data <- c()
+			for (clus.id in 1:length(clusters)) {
+				clus.samples <- clusters[[clus.id]]
+				selected.models <- names(models.match[[clus.id]][1:top.clines])
+				data <- cbind(data, scaled.vipermat[features,clus.samples], model.viper[features, selected.models])
+			}
+
+			#
+			cline.total.counts <- sapply(unique(names(unlist(models.match))), function(cline) {
+				sum(sapply(1:6, function(m) {
+					r <- 0	
+					if (cline %in% names(models.match[[m]])) {
+						r <- models.match[[m]][cline]
+					} 
+					r
+				}))
+			
+			})
+
+			# hugo gene names	
+			rownames(data) <- map.entrez(rownames(data))
+			
+			pdf(output.file, width=16, height=16)
+			heatmap.2(data, 
+				col=c(fn1(25), fn2(25)), 
+				colsep=F, 
+				rowsep=F, 
+				distfun=corDist, 
+				tracecol=NA, 
+				Colv=FALSE, 
+				Rowv=FALSE, 
+				margins=c(10,5), 
+				keysize=0.5,
+			breaks=seq(-3,3, length.out=51))
+			dev.off()
+
+		},
+		#' Plot the viper matrix of the best matching cell lines against the MEAN of each cluster
+		#' 
+		heatmap.patientMean = function(output.file) {
 
 			# features to select
 			features <- unlist(checkpoints)
