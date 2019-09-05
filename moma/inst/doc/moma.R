573c94d6e2a074c00015b1f1980b799c08ee7873
epaull <ep2835@c2b2.columbia.edu>
2019-09-05 14:18:09 -0400

    Move to the right directory

--- moma/inst/doc/moma.R
+++
@@ -1,9 +0,0 @@
-### R code from vignette source 'moma.Rnw'
-
-###################################################
-### code chunk number 2: moma.Rnw:36
-###################################################
-library(moma)
-
-
-
